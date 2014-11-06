df35cb5559b33e4455bdc0c7ac92725488c115ed
chen arbel <arbel.chenlia@gmail.com>
2014-11-07 01:47:20 +0200

    task2

--- src/com/mta/chen/Mta_java_chenServlet.java
+++ src/com/mta/chen/Mta_java_chenServlet.java
@@ -1,17 +1,23 @@
 package com.mta.chen;
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class Mta_java_chenServlet extends HttpServlet
 {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	@SuppressWarnings("unused")
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws IOException {
 		resp.setContentType("text/html");
-		int num1, num2, num3;
+		int num1;
+		int num2;
+		int num3;
 		num1 = 3;
 		num2 = 4;
 		num3 = 7;
-		int result = (num1 + num2) * num3;
-		resp.getWriter().println("Hello, chen");
+		int result =(num1+num2) *num3;
+		String h = new String("<h1>Result of "+num1+"+"+num2+"*"+num3+"="+result+"</h1>");
+		resp.getWriter().println("h");
 	}
 }
