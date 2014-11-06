1a1df0f8c55b5f5baaac8fcda091dd1dfea47db0
chen arbel <arbel.chenlia@gmail.com>
2014-11-07 01:32:29 +0200

    sec commit

--- src/com/mta/chen/Mta_java_chenServlet.java
+++ src/com/mta/chen/Mta_java_chenServlet.java
@@ -3,9 +3,15 @@ import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class Mta_java_chenServlet extends HttpServlet {
+public class Mta_java_chenServlet extends HttpServlet
+{
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		int num1, num2, num3;
+		num1 = 3;
+		num2 = 4;
+		num3 = 7;
+		int result = (num1 + num2) * num3;
+		resp.getWriter().println("Hello, chen");
 	}
 }
